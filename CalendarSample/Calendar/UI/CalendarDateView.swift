bb6bc8d580c4abc62eeebfbf550205f614187738
me990928 <me990928@gmail.com>
2024-09-10 05:45:28 +0900

    めもの追加

--- CalendarSample/Calendar/UI/CalendarDateView.swift
+++ CalendarSample/Calendar/UI/CalendarDateView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+/// カレンダーの日付UI
+///  - 繰り返し表示でカレンダーを描写
+///  - 簡易的なテキストを表示
 struct CalendarDateView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
