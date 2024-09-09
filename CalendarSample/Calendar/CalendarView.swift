36f5a75547ad736de13691933d3d1b8b234ebb3d
me990928 <me990928@gmail.com>
2024-09-10 06:08:06 +0900

    カレンダービューのメイン画面の追加

--- CalendarSample/Calendar/CalendarView.swift
+++ CalendarSample/Calendar/CalendarView.swift
@@ -10,7 +10,54 @@ import SwiftUI
 /// カレンダーのメイン
 struct CalendarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            ZStack {
+                HStack{
+                    
+                    Group{
+                        // 先月、来月の選択
+                        Button(action: {}, label: {
+                            Image(systemName: "chevron.backward")
+                        })
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Image(systemName: "chevron.forward")
+                        })
+                    }
+                    
+                    Spacer()
+                    
+                }.padding()
+                
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    Text("2024年 ").bold()
+                    Text("9月").bold()
+                    
+                    Spacer()
+                    
+                }
+            }
+            
+            GeometryReader { geo in
+                HStack(spacing: 0,content: {
+                    Grid(horizontalSpacing: 0) {
+                        GridRow {
+                            Text("日").frame(width: geo.size.width / 7).foregroundStyle(.red)
+                            Text("月").frame(width: geo.size.width / 7)
+                            Text("火").frame(width: geo.size.width / 7)
+                            Text("水").frame(width: geo.size.width / 7)
+                            Text("木").frame(width: geo.size.width / 7)
+                            Text("金").frame(width: geo.size.width / 7)
+                            Text("土").frame(width: geo.size.width / 7).foregroundStyle(.blue)
+                        }
+                    }
+                })
+            }
+        }
     }
 }
 
