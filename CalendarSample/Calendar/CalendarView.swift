1fd1101ab73c46dbc98df1ccb0da2e2920b7ff8a
me990928 <me990928@gmail.com>
2024-09-13 17:41:37 +0900

    上下スワイプで変更

--- CalendarSample/Calendar/CalendarView.swift
+++ CalendarSample/Calendar/CalendarView.swift
@@ -111,6 +111,28 @@ struct CalendarView: View {
         }.onAppear(){
             calendarModel.createCalendar(current: currentDate)
         }
+        .gesture(
+            DragGesture()
+                .onEnded { value in
+                    if value.translation.height < -50 {
+                        // 上スワイプ
+                        print("上")
+                        withAnimation(){
+                            currentDate = calendarModel.upCalendarMonth(current: currentDate)
+                            calendarModel.calendarArr = []
+                            calendarModel.createCalendar(current: currentDate)
+                        }
+                    } else if value.translation.height > 30 {
+                        // 下スワイプ
+                        print("下")
+                        withAnimation(){
+                            currentDate = calendarModel.downCalendarMonth(current: currentDate)
+                            calendarModel.calendarArr = []
+                            calendarModel.createCalendar(current: currentDate)
+                        }
+                    }
+                }
+        )
     }
     
     /// 年月のテキストを変更
